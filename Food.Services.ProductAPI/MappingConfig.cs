f45ada54d287038f1d5228da182752acb6bd100e
Gray Pham <hunghtk456@gmail.com>
2022-04-15 00:10:50 +0700

    Repository's Product

---
+++ Food.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper; // using to Map
+using Food.Services.ProductAPI.Model;
+using Food.Services.ProductAPI.Model.Dtos;
+
+namespace Food.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
--- Food.Services.ProductAPI/Program.cs
+++ Food.Services.ProductAPI/Program.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Configuration;
+using AutoMapper;
+using Food.Services.ProductAPI;
+using Food.Services.ProductAPI.Repository;
 
 
 
@@ -26,8 +29,14 @@ string connString = builder.Configuration.GetConnectionString("DefaultConnection
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    
 
-}); 
+});
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
