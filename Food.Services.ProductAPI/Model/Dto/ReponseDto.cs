f89561a98b6c65f1e32391f0e0a635dbef66f4f4
Gray Pham <hunghtk456@gmail.com>
2022-04-14 12:41:15 +0700

    interface IProductRepository

---
+++ Food.Services.ProductAPI/Model/Dto/ReponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Food.Services.ProductAPI.Model.Dto
+{
+    public class ReponseDto
+    {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; } 
+    }
+}
---
+++ Food.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Food.Services.ProductAPI.Model.Dtos;
+
+namespace Food.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductsById(int id);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
