513b4c5d3465592f9297a25d981277692d04872f
Gray Pham <hunghtk456@gmail.com>
2022-04-17 19:18:20 +0700

    012 Base Service Interface and API Request Model

---
+++ Food.Web/Services/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using Food.Web.Models;
+using Food.Web.Services.IServices;
+
+namespace Food.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ReponseDto reponseDto { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Food.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿using Food.Web.Models;
+
+namespace Food.Web.Services.IServices
+{
+    public interface IBaseService:IDisposable
+    {
+        ReponseDto reponseDto { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+        
+    }
+}
